099b9680a17904612e5d08f804df2721e3c081bb
Luciana Sugeta <lulusugeta@gmail.com>
2021-07-14 14:28:57 -0400

    commit tree 1865f9d2529c4a629e0447c94859d2d7d4613d80

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,6 +1,53 @@
 class City < ActiveRecord::Base
   has_many :neighborhoods
   has_many :listings, :through => :neighborhoods
+  has_many :reservations, through: :listings
+
+  def city_openings(start_date, end_date)
+    available_listings = []
+
+    self.listings.each do |listing|
+      available = true
+      listing.reservations.each do |reservation|
+        if start_date.to_date.between?(reservation.checkin, reservation.checkout) || end_date.to_date.between?(reservation.checkin, reservation.checkout)
+          available = false
+        end
+      end
+
+      if available == true
+        available_listings << listing
+      end
+
+    end
+    available_listings
+  end
+
+  def self.highest_ratio_res_to_listings
+    current_highest = 0.0
+    highest_city = self.new
+
+    self.all.each do |city|
+      ratio = (city.reservations.count / city.listings.count).to_f
+      if ratio > current_highest
+        current_highest = ratio
+        highest_city = city
+      end
+    end
+    highest_city
+  end
+
+  def self.most_res
+    current_highest = 0
+    highest_city = self.new
+  
+  self.all.each do |city|
+    if city.reservations.count > current_highest
+      highest_city = city
+      current_highest = city.reservations.count
+    end
+  end
+  highest_city
+  end
 
 end
 
--- app/models/listing.rb
+++ app/models/listing.rb
@@ -5,4 +5,33 @@ class Listing < ActiveRecord::Base
   has_many :reviews, :through => :reservations
   has_many :guests, :class_name => "User", :through => :reservations
   
+  validates :address, :listing_type, :title, :description, :price, :neighborhood_id, presence: true
+
+  after_create :change_host_status
+  after_destroy :host_false
+
+  def change_host_status
+    host.host = true
+    host.save
+  end
+
+  def host_false
+    if host.listings.blank?
+      host.host = false
+      host.save
+    end
+  end
+
+  def average_review_rating
+    #total number of reviews
+    #sum of all the review ratings
+    #divide those two
+
+    total_sum = 0
+    self.reviews.each do |review|
+      total_sum += review.rating
+    end
+    total_sum.to_f / self.reviews.count.to_f
+  end
+
 end
